d86bf7d6b2b2792bbc0970490db685b601043953
João David <joaodavid512@gmail.com>
2020-05-06 18:11:26 +0100

    added type check exceptions

---
+++ CasualCompiler_49448/src/ast/exception/DuplicateVarAssignException.java
@@ -0,0 +1,10 @@
+package ast.exception;
+
+public class DuplicateVarAssignException extends TypeCheckException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ CasualCompiler_49448/src/ast/exception/TypeCheckException.java
@@ -0,0 +1,11 @@
+package ast.exception;
+
+public class TypeCheckException extends Exception {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
